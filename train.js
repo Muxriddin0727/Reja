836fcf55bda2c5d934797dd1bddb695fd0bd907d
Muxriddin0727 <abduvohidov.muxriddin0727@gmail.com>
2023-05-13 19:42:10 +0900

    feat:yangi

--- train.js
+++ train.js
@@ -1,16 +1,53 @@
+vazifa
 
-console.log("Jack Ma maslahatlari");
+const moment = require("moment");
+const data = moment();
 
-const list = [
-  "Yaxshi talaba bo'ling", // 0-20
-  "To'g'ri boshliq tanlang va ko'proq xato qiling", // 20-30
-  "O'zingizga ishlashni boshlang", // 30-40
-  "Siz kuchli bo'lgan narsalarni qiling", // 40-50
-  "Yoshlarga investitsiya qiling", // 50-60
-  "Endi dam oling, foydasi yo'q endi", // 60
-];
-//Callback functions
-function maslahatBering(a, callback) {
+class Shop {
+  constructor(non, cola, lagmon) {
+    this.non = non;
+    this.cola = cola;
+    this.lagmon = lagmon;
+  }
+
+
+  result() {
+    console.log(
+      `Hozir ${data.add(10, "minutes").format("HH:mm")}da ${this.non}ta non, ${ this.lagmon }ta lagmon va ${this.cola}ta cola bor.`
+    );
+  }
+
+  sell(a, b) {
+    this.non -= b;
+  }
+
+  get(a, b) {
+    this.cola += b;
+  }
+}
+
+const shop = new Shop(4, 5, 2);
+shop.result();
+shop.sell("non", 3);
+shop.get("cola", 4);
+shop.result();
+
+
+
+
+
+//console.log("Jack Ma maslahatlari");
+
+//const list = [
+ // "Yaxshi talaba bo'ling", // 0-20
+  //"To'g'ri boshliq tanlang va ko'proq xato qiling", // 20-30
+ // "O'zingizga ishlashni boshlang", // 30-40
+ // "Siz kuchli bo'lgan narsalarni qiling", // 40-50
+ // "Yoshlarga investitsiya qiling", // 50-60
+ // "Endi dam oling, foydasi yo'q endi", // 60
+//];
+////Callback functions
+//function maslahatBering(a, callback) {
     if (typeof a !== "number") callback("Iltimos raqam yozing", null);
   else if (a <= 20) callback(null, list[0]);
    else if (a > 20 && a <= 30) callback(null, list[1]);
@@ -22,16 +59,16 @@ else if (a > 40 && a <= 50) callback(null, list[3]);
        callback(null, list[5]);
      }, 1000);
 }
-  }
+ // }
 
-  console.log("passed here 0");
-  maslahatBering(65, (err, data) => {
-  if(err) console.log('ERROR:', err);
-   else {
-  console.log('javob:', data);
-    }
- });
-  console.log("passed here 1");
+ // console.log("passed here 0");
+  //maslahatBering(65, (err, data) => {
+  //if(err) console.log('ERROR:', err);
+  // else {
+ // console.log('javob:', data);
+   // }
+ //});
+  //console.log("passed here 1");
 
 
  //acynchronous functions
