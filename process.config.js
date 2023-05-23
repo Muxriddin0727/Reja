5fad63b0b02a884bb8a6b937f54b567c676200dc
Muxriddin0727 <abduvohidov.muxriddin0727@gmail.com>
2023-05-24 00:04:13 +0900

    mongodb error fix

--- process.config.js
+++ process.config.js
@@ -1,18 +1,18 @@
-module.exports = {
-  apps: [
-      {
-           name: "complex-app",
-           cwd: "./",
-           script: "./server.js",
-           watch: false,
-          //env_production: {
-            // NODE_ENV: "PRODUCTION",
+//module.exports = {
+// apps: [
+ // {
+// name: "complex-app",
+//         cwd: "./",
+//        script: "./server.js",
+//          watch: false,
+         //env_production: {
+         // NODE_ENV: "PRODUCTION",
             //    },
-            //env_development: {
+           //env_development: {
             // NODE_ENV: "DEVELOPMENT",
             // },
-            instances: 1,
-          exec_mode: "cluster",
-        },s
-    ],
-};
+//           instances: 1,
+//         exec_mode: "cluster",
+//       },
+//   ],
+//};
