fe028ff5c80c208b8572ed7f24a033ef9b41b91c
Muxriddin0727 <abduvohidov.muxriddin0727@gmail.com>
2023-05-25 19:24:08 +0900

    Task-e added

--- train.js
+++ train.js
@@ -1,36 +1,65 @@
-vazifa
+//vazifa
+//TASK-E
 
-const moment = require("moment");
-const data = moment();
+const primeNumber= function(number) {
+  if (number <=1) return false ;
+    for (let i = 4; i <=number; i++) {
+      if (number % i === 0) {
+        return false;
+      }
+    }
+      
+  return true;
+};
 
-class Shop {
-  constructor(non, cola, lagmon) {
-    this.non = non;
-    this.cola = cola;
-    this.lagmon = lagmon;
-  }
+const result = primeNumber(9);
+console.log(result);
 
 
-  result() {
-    console.log(
-      `Hozir ${data.add(10, "minutes").format("HH:mm")}da ${this.non}ta non, ${ this.lagmon }ta lagmon va ${this.cola}ta cola bor.`
-    );
-  }
 
-  sell(a, b) {
-    this.non -= b;
-  }
 
-  get(a, b) {
-    this.cola += b;
-  }
-}
 
-const shop = new Shop(4, 5, 2);
-shop.result();
-shop.sell("non", 3);
-shop.get("cola", 4);
-shop.result();
+
+
+
+
+
+
+
+
+
+
+//const moment = require("moment");
+//const data = moment();
+
+//class Shop {
+ // constructor(non, cola, lagmon) {
+//this.non = non;
+   // this.cola = cola;
+   // this.lagmon = lagmon;
+  //}
+
+
+ // result() {
+ //   console.log(
+//      `Hozir ${data.add(10, "minutes").format("HH:mm")}da ${this.non}ta non, ${ this.lagmon }ta lagmon va ${this.cola}ta cola bor.`
+//    );
+ // }
+
+//  sell(a, b) {
+ //   this.non -= b;
+//  }
+
+ // get(a, b) {
+   // this.cola += b;
+//}
+//}
+
+//const shop = new Shop(4, 5, 2);
+//shop.result();
+//shop.sell("non", 3);
+//shop.get("cola", 4);
+//shop.result();
 
 
 
@@ -48,17 +77,17 @@ shop.result();
 //];
 ////Callback functions
 //function maslahatBering(a, callback) {
-    if (typeof a !== "number") callback("Iltimos raqam yozing", null);
-  else if (a <= 20) callback(null, list[0]);
-   else if (a > 20 && a <= 30) callback(null, list[1]);
-    else if (a > 30 && a <= 40) callback(null, list[2]);
-else if (a > 40 && a <= 50) callback(null, list[3]);
-    else if (a > 50 && a <= 60) callback(null, list[4]);
-    else {
-     setInterval(function () {
-       callback(null, list[5]);
-     }, 1000);
-}
+ //   if (typeof a !== "number") callback("Iltimos raqam yozing", null);
+ // else if (a <= 20) callback(null, list[0]);
+ //  else if (a > 20 && a <= 30) callback(null, list[1]);
+ //   else if (a > 30 && a <= 40) callback(null, list[2]);
+//else if (a > 40 && a <= 50) callback(null, list[3]);
+ //   else if (a > 50 && a <= 60) callback(null, list[4]);
+  //  else {
+ //    setInterval(function () {
+ //      callback(null, list[5]);
+ //    }, 1000);
+//}
  // }
 
  // console.log("passed here 0");
